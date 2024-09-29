4589d480218f7f8db053e3d9b33e29fbd0bb282b
paukvlad <82984225+paukvlad@users.noreply.github.com>
2024-09-28 18:26:14 -0700

    working aevb and vasca

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,6 @@
 import src.data.synthetic as synthetic
 import src.data.torchvision as torchvision
 import src.data.hyperspectral as hyperspectral
+import src.data.real_tabular as real_tabular
+import src.data.synthetic_cnae as synthetic_cnae
+import src.data.data_cnae as data_cnae
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,3 +2,4 @@ import src.model.aevb as AEVB
 import src.model.prism as PRISM
 import src.model.vasca as VASCA
 import src.model.nisca as NISCA
+import src.model.cnae as CNAE
