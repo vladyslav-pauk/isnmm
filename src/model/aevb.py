7a48db560455e38242967b85ad72b750820250dc
paukvlad <82984225+paukvlad@users.noreply.github.com>
2024-09-28 18:26:04 -0700

    working aevb and vasca

--- src/model/aevb.py
+++ src/model/aevb.py
@@ -2,22 +2,30 @@ import torch
 import torch.nn as nn
 import torchmetrics
 import torch.nn.functional as F
+import torch.optim as optim
 
 from src.modules.vae_module import VAEModule
 from src.modules.network import FCNConstructor
 
 
 class Model(VAEModule):
-    def __init__(self, encoder=None, decoder=None, train_config=None, **kwargs):
-        super().__init__(encoder=encoder, decoder=decoder, **train_config)
+    def __init__(self, encoder=None, decoder=None, model_config=None, optimizer_config=None, **kwargs):
+        super().__init__(encoder=encoder, decoder=decoder, **model_config)
 
         self.metrics = torchmetrics.MetricCollection({})
+        self.optimizer_config = optimizer_config
+
+        self.log_monitor = {
+            "monitor": "validation_loss",
+            "mode": "min"
+        }
 
     def reparameterize(self, params, mc_samples):
         mean, log_var = params
         std = torch.exp(0.5 * log_var)
         eps = torch.randn_like(std)
-        return eps.mul(std).add_(mean)
+        latent_mc_sample = eps.mul(std).add_(mean)
+        return latent_mc_sample
 
     @staticmethod
     def loss_function(data, model_output):
@@ -27,16 +35,29 @@ class Model(VAEModule):
         kld = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp()) / x.size(0)
         return {"cross-entropy": bce, "kl_divergence": kld}
 
+    def configure_optimizers(self):
+        lr = self.optimizer_config["lr"]
+        lr_th = lr["th"]
+        lr_ph = lr["ph"]
+        optimizer_class = getattr(optim, self.optimizer_config["name"])
+        optimizer = optimizer_class([
+            {'params': self.encoder.parameters(), 'lr': lr_ph},
+            {'params': self.decoder.parameters(), 'lr': lr_th}
+        ], **self.optimizer_config["params"])
+        return optimizer
+
     def update_metrics(self, data, model_output, labels):
         pass
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_dim, latent_dim, config_encoder):
+    def __init__(self, config):
         super().__init__()
+        self.config = config
 
-        self.mu_network = FCNConstructor(input_dim, latent_dim, **config_encoder)
-        self.log_var_network = FCNConstructor(input_dim, latent_dim, **config_encoder)
+    def construct(self, latent_dim, observed_dim):
+        self.mu_network = FCNConstructor(observed_dim, latent_dim, **self.config)
+        self.log_var_network = FCNConstructor(observed_dim, latent_dim, **self.config)
 
     def forward(self, x):
         mu = self.mu_network.forward(x)
@@ -45,10 +66,12 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, latent_dim, output_dim, config_decoder):
+    def __init__(self, config):
         super().__init__()
+        self.config = config
 
-        self.network = FCNConstructor(latent_dim, output_dim, **config_decoder)
+    def construct(self, latent_dim, observed_dim):
+        self.network = FCNConstructor(latent_dim, observed_dim, **self.config)
 
     def forward(self, z):
         return self.network.forward(z)
