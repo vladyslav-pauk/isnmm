a5fe235e3e69191de3d6af535a06917a56889195
paukvlad <82984225+paukvlad@users.noreply.github.com>
2024-10-02 20:15:12 -0700

    all work

--- src/generate_data.py
+++ src/generate_data.py
@@ -6,7 +6,7 @@ import src.modules.distribution as distribution_package
 
 
 if __name__ == "__main__":
-    data_model_name = 'noisy_nmm'
+    data_model_name = 'nnmm'
     config = load_experiment_config('nonlinearity_removal', data_model_name)
 
     linear_mixture_matrix = torch.randn(config["observed_dim"], config["latent_dim"])
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     generative_model = getattr(distribution_package, config["model_name"])
     model = generative_model(linear_mixture_matrix, data_model_name, **config)
 
-    sample, (latent, linearly_mixed, noiseless) = model.sample(torch.Size([10000]))
+    sample, (latent, linearly_mixed, noiseless) = model.sample()
     model.save_data()
 
     model.plot_nonlinearities()
